6a76c355a5afb1b31baf3f08bdc41c75dd9ef525
PietraKruger <Pietrapikruger11@gmail.com>
2025-06-11 16:48:44 -0300

    Add files via upload

---
+++ idadeDoCachorro.js
@@ -0,0 +1,13 @@
+let idade = Number (prompt ("Qual a idade atual do cachorro?"))
+
+let idadeDeCachorro = idade * 7
+
+alert ("Em idade de cachorro, isso dá: " + idadeDeCachorro + " anos")
+
+if (idadeDeCachorro >= 65){
+alert ("Ele já pode se aposentar!")
+}
+
+else{
+    alert ("Que pena, ainda não pode se aposentar;(")
+}
---
+++ jucaPalestras.js
@@ -0,0 +1,21 @@
+let numero = Number (prompt("Qual o número da palestra que deseja ir?"))
+
+ if(numero === 1){
+alert("Animações com Scratch, laboratório 305 às 19h")
+}
+else if(numero === 2){
+    alert("Scratch para gamers, laboratório 512 às 20h")
+    }
+else if(numero === 3){
+    alert("JavaScript para leigos, laboratório 101 às 19h")
+        }
+else if(numero === 4){
+    alert("Tópicos avançados de JavaScript, laboratório 305 às 20h")
+        }        
+else if(numero === 5){
+    alert("Vida e carreira, auditório às 21h")
+        }    
+        
+else{
+    alert("Número inválido. Digite novamente!")
+}

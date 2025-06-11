b42f73c918ba2deb610d56062a96f6a601e8c31a
PietraKruger <Pietrapikruger11@gmail.com>
2025-06-11 17:08:02 -0300

    Add files via upload

---
+++ jucaPassou.js
@@ -0,0 +1,14 @@
+let nota1 = Number (prompt("Digite a primeira nota:"))
+let nota2 = Number (prompt("Digite a segunda nota:"))
+
+if (nota1 + nota2 / 2 >= 7){
+    alert("Você passou!")
+}
+
+else if (nota1 + nota2 /2 <7){
+    alert("Você não passou, faça a recuperação;(")
+}
+
+else{
+    alert("Erro! Digite novamente:")
+}

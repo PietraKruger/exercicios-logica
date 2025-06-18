b0b338a26e672fbc5ebc85e139ddc890ada9084a
PietraKruger <Pietrapikruger11@gmail.com>
2025-06-18 17:06:14 -0300

    Add files via upload

---
+++ feiraJuca.js
@@ -0,0 +1,9 @@
+let fruta = Number (prompt("Digite a quantidade de maçãs que você quer:"))
+
+if (fruta <= 11) {
+    alert("O valor de cada uma é de R$ 0,30")
+}
+
+else {
+    alert("O valor de cada uma é de R$ 0,25")
+}

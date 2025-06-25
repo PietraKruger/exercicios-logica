916990639a5c11738f569b2a5c9beac646dc60cc
PietraKruger <Pietrapikruger11@gmail.com>
2025-06-25 17:02:16 -0300

    Update starUber1.js

--- starUber1.js
+++ starUber1.js
@@ -1,28 +1,24 @@
 let distancia = Number (prompt ("Insira a distancia em quilometros da proxima corrida:"))
 tempo = (distancia / 300000)
 
-if (tempo < 60 && tempo > 0){
 alert ("O tempo da viagem é " + tempo + " segundos"  )
-}
 
-else if (tempo >= 60 && tempo < 3600 ) {
-    alert ("O tempo da viagem é " + (tempo * 60) + " minutos"  )
+if (tempo >= 60 && tempo < 3600 ) {
+    alert ("O tempo da viagem é " + (tempo / 60) + " minutos"  )
 }
 
 else if  (tempo >= 3600 && tempo < 86400){
-    alert ("O tempo da viagem é " + (tempo * 3600) + " horas"  )
+    alert ("O tempo da viagem é " + (tempo / 3600) + " horas"  )
 }
 
  else if  (tempo >= 86400 && tempo < 2592000){
-alert ("O tempo da viagem é " + (tempo * 86400) + " dias"  )
+alert ("O tempo da viagem é " + (tempo / 86400) + " dias"  )
 }
 
 else if  (tempo >= 2592000 && tempo < 946080000){
-alert ("O tempo da viagem é " + (tempo * 2592000) + " meses"   )
+alert ("O tempo da viagem é " + (tempo / 2592000) + " meses"   )
 } 
 
 else  {
- alert ("O tempo da viagem é " + (tempo * 946080000) + " anos"  )
+ alert ("O tempo da viagem é " + (tempo / 946080000) + " anos"  )
 }
-
-
